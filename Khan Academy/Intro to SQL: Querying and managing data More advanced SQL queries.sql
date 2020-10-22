233b2abc1e53ca5fe5387de6adec1b527e2ab611
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-10-22 11:52:48 +0100

    Update Intro to SQL: Querying and managing data More advanced SQL queries.sql

--- Khan Academy/Intro to SQL: Querying and managing data More advanced SQL queries.sql
+++ Khan Academy/Intro to SQL: Querying and managing data More advanced SQL queries.sql
@@ -71,3 +71,41 @@ SELECT title FROM songs WHERE mood = 'epic' AND released > 1990 AND duration < 2
 Querying IN subqueries
 */
 
+CREATE TABLE exercise_logs
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    minutes INTEGER, 
+    calories INTEGER,
+    heart_rate INTEGER);
+
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("biking", 30, 100, 110);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("biking", 10, 30, 105);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("dancing", 15, 200, 120);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("tree climbing", 30, 70, 90);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("tree climbing", 25, 72, 80);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("rowing", 30, 70, 90);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("hiking", 60, 80, 85);
+
+SELECT * FROM exercise_logs WHERE type = "biking" OR type = "hiking" OR type = "tree climbing" OR type = "rowing";
+
+-- Working with IN 
+SELECT * FROM exercise_logs WHERE type IN ("biking", "hiking", "tree climbing", "rowing");
+
+-- Working with Sub QUeries
+
+INSERT INTO drs_favorites(type, reason) VALUES ("biking", "Improves endurance and flexibility.");
+INSERT INTO drs_favorites(type, reason) VALUES ("hiking", "Increases cardiovascular health.");
+
+-- Selects the type column from the drs_favourites table
+SELECT type FROM drs_favorites;
+
+-- Selects the type from the drs_favorites table so the query does not break when the type is modified
+SELECT * FROM exercise_logs WHERE type IN (
+    SELECT type FROM drs_favorites);
+    
+SELECT * FROM exercise_logs WHERE type IN (
+    SELECT type FROM drs_favorites WHERE reason = "Increases cardiovascular health");
+    
+-- Using LIKE which can be used with some wildcard characters such as *, ?, [], !, -, #
+SELECT * FROM exercise_logs WHERE type IN (
+    SELECT type FROM drs_favorites WHERE reason LIKE "%cardiovascular%");
