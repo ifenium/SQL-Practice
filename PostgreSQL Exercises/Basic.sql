066b1be8091083ab36b2e654590d7e834927e4eb
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-10-29 20:25:50 +0100

    Create Basic.sql

---
+++ PostgreSQL Exercises/Basic.sql
@@ -0,0 +1,9 @@
+--Queries based on exercises from PostgreSQL Exercises (https://pgexercises.com/questions/basic/)
+
+/*
+Retrieve everything from a table
+*/
+-- Question
+How can you retrieve all the information from the cd.facilities table?
+-- Answer
+SELECT * FROM cd.facilities;
