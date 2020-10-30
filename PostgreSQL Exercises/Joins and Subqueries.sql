f2dac1d89e4e99f3340ee7960a7dab65c1a13e39
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-10-30 22:07:26 +0100

    Update Joins and Subqueries.sql

--- PostgreSQL Exercises/Joins and Subqueries.sql
+++ PostgreSQL Exercises/Joins and Subqueries.sql
@@ -30,11 +30,11 @@ SELECT bks.starttime
 --or
 SELECT  bks.starttime 
 	FROM cd.bookings AS bks,
-				cd.members AS mems
+			cd.members AS mems
 	WHERE
-				mems.surname ='Farrell' 
-				AND mems.firstname = 'David' 
-				AND bks.memid = bks.memid;
+			mems.surname ='Farrell' 
+			AND mems.firstname = 'David' 
+			AND bks.memid = bks.memid;
 
 /*
 Work out the start times of bookings for tennis courts
@@ -110,32 +110,76 @@ SELECT DISTINCT mems.firstname || ' ' || mems.surname AS member, facs.name AS fa
 ORDER BY member, facility     
 
 /*
-
+Produce a list of costly bookings
 -- Question:
-
+How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different 
+costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. Include in your output the name of the facility, the name 
+of the member formatted as a single column, and the cost. Order by descending cost, and do not use any subqueries.
 */
 -- Answer
-
+SELECT mems.firstname || ' ' || mems.surname AS member, facs.name AS facility, 
+	CASE
+		WHEN mems.memid = 0 THEN bks.slots*facs.guestcost
+		ELSE bks.slots*facs.membercost 
+	END AS cost
+	FROM cd.members AS mems
+		INNER JOIN cd.bookings AS bks 
+			ON mems.memid = bks.memid
+		INNER JOIN cd.facilities AS facs
+			ON bks.facid = facs.facid
+	WHERE 
+		bks.starttime >= '2012-09-14' AND 
+		bks.starttime < '2012-09-15' 
+		AND (
+		  (mems.memid = 0 and bks.slots*facs.guestcost > 30) OR 
+		  (mems.memid != 0 and bks.slots*facs.membercost > 30))
+		  
+ORDER BY cost DESC;
 
 /*
-
+Produce a list of all members, along with their recommender, using no joins.
 -- Question:
-
+How can you output a list of all members, including the individual who recommended them (if any), 
+without using any joins? Ensure that there are no duplicates in the list, and that each firstname + surname pairing is formatted as a column and ordered.
 */
 -- Answer
-
+SELECT DISTINCT mems.firstname || ' ' || mems.surname AS member, 
+		(SELECT recs.firstname || ' ' || recs.surname AS recommender 
+			FROM cd.members AS recs
+			WHERE recs.memid = mems.recommendedby
+		)
+FROM cd.members as mems
+ORDER BY member;
 
 /*
-
+Produce a list of costly bookings, using a subquery
 -- Question:
-
+The Produce a list of costly bookings exercise contained some messy logic: we had to calculate the booking cost in both the WHERE clause and the CASE statement. 
+Try to simplify this calculation using subqueries. For reference, the question was:
+How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different costs to 
+members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. Include in your output the name of the facility, the name of the member 
+formatted as a single column, and the cost. Order by descending cost.
 */
 -- Answer
-
-
-/*
-
--- Question:
-
-*/
--- Answer
+SELECT member, facility, cost
+FROM (
+  SELECT
+  mems.firstname || ' ' || mems.surname AS member, facs.name AS facility,
+  	CASE
+  		WHEN mems.memid = 0 THEN
+  			bks.slots*facs.guestcost
+ 		ELSE 
+  			bks.slots*facs.membercost
+  	END AS cost
+  	FROM
+  		cd.members AS mems
+  		INNER JOIN cd.bookings bks
+				ON mems.memid = bks.memid
+			INNER JOIN cd.facilities facs
+				ON bks.facid = facs.facid
+  	WHERE 
+  		bks.starttime >= '2012-09-14' and
+			bks.starttime < '2012-09-15'
+  ) AS bookings
+  WHERE cost > 30 
+ORDER BY COST;
