9f6025bcd5eab72e60ed1ec1f67e039fd403f31c
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-10-30 09:05:53 +0100

    Update Basic.sql

--- PostgreSQL Exercises/Basic.sql
+++ PostgreSQL Exercises/Basic.sql
@@ -2,8 +2,29 @@
 
 /*
 Retrieve everything from a table
-*/
--- Question
+Question: 
 How can you retrieve all the information from the cd.facilities table?
+*/ 
 -- Answer
 SELECT * FROM cd.facilities;
+
+/*
+Retrieve specific columns from a table
+Question: 
+You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
+*/
+-- Answer
+SELECT name, membercost FROM cd.facilities;
+
+
+/*
+
+-- Question:
+*/
+-- Answer
+
+/*
+
+-- Question:
+*/
+-- Answer
