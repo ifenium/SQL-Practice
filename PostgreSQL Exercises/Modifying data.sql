8a2f349e6ae85c1286340004d834a9901b189483
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-10-31 21:47:14 +0100

    Update Modifying data.sql

--- PostgreSQL Exercises/Modifying data.sql
+++ PostgreSQL Exercises/Modifying data.sql
@@ -13,13 +13,26 @@ The club is adding a new facility - a spa. We need to add it into the facilities
 facid: 9, Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
 */ 
 -- Answer
+INSERT INTO cd.facilities
+	(facid, Name, membercost, guestcost, initialoutlay, monthlymaintenance)
+	VALUES(9, 'Spa', 20, 30, 100000, 800);
+--or 
+INSERT INTO cd.facilities VALUES(9, 'Spa', 20, 30, 100000, 800);
 
 /*
-
+Insert multiple rows of data into a table
 Question:
-
+In the previous exercise, you learned how to add a facility. Now you're going to add multiple facilities in one command. 
+Use the following values:
+  facid: 9, Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
+  facid: 10, Name: 'Squash Court 2', membercost: 3.5, guestcost: 17.5, initialoutlay: 5000, monthlymaintenance: 80.
 */
 -- Answer 
+INSERT INTO cd.facilities
+	(facid, Name, membercost, guestcost, initialoutlay, monthlymaintenance)
+	VALUES
+		(9, 'Spa', 20, 30, 100000, 800),
+		(10, 'Squash Court 2', 3.5, 17.5, 5000, 80);
 
 /*
 
