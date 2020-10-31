d55e618f96ffef5452ee87a9743dfce00a38c9db
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-10-31 21:32:03 +0100

    Create Modifying data.sql

---
+++ PostgreSQL Exercises/Modifying data.sql
@@ -0,0 +1,43 @@
+--Queries based on exercises from PostgreSQL Exercises (https://pgexercises.com/questions/basic/)
+
+/*
+Querying data is all well and good, but at some point you're probably going to want to put data into your database! 
+This section deals with inserting, updating, and deleting information. Operations that alter your data like this are 
+collectively known as Data Manipulation Language, or DML.
+*/
+
+/*
+Insert some data into a table
+Question: 
+The club is adding a new facility - a spa. We need to add it into the facilities table. Use the following values:
+facid: 9, Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
+*/ 
+-- Answer
+
+/*
+
+Question:
+
+*/
+-- Answer 
+
+/*
+
+Question:
+
+*/
+-- Answer 
+
+/*
+
+Question:
+
+*/
+-- Answer 
+
+/*
+
+Question:
+
+*/
+-- Answer 
