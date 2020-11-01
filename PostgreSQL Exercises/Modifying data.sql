720fe61e6842215c766bab4237287e381f23b7da
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-01 22:08:00 +0100

    Update Modifying data.sql

--- PostgreSQL Exercises/Modifying data.sql
+++ PostgreSQL Exercises/Modifying data.sql
@@ -47,13 +47,44 @@ UPDATE cd.facilities
 	WHERE facid = 1;
 
 /*
+Update multiple rows and columns at the same time
+Question:
+We want to increase the price of the tennis courts for both members and guests. Update the costs to be 6 for members, and 30 for guests.
+*/
+-- Answer 
+UPDATE cd.facilities
+   SET 
+	 membercost=6,
+	 guestcost=30
+   WHERE facid IN (0,1);
+
+/*
+Update a row based on the contents of another row
+Question:
+We want to alter the price of the second tennis court so that it costs 10% more than the first one. 
+Try to do this without using constant values for the prices, so that we can reuse the statement if we want to.
+*/
+-- Answer 
+UPDATE cd.facilities
+	SET membercost = (1.1*(SELECT membercost FROM cd.facilities WHERE facid=0)),
+		guestcost = (1.1*(SELECT guestcost FROM cd.facilities WHERE facid=0))
+WHERE facid=1
+
+-or
+UPDATE cd.facilites 
+	SET membercost = (SELECT membercost*1.1 FROM cd.faclities WHERE facid=0),
+		guestcost = (SELECT guestcost*1.1 FROM cd.facilities WHERE facid=0)
+
+
+/*
 
 Question:
 
 */
 -- Answer 
 
-/*
+
+*
 
 Question:
 
