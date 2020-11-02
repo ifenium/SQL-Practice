5b61462c15436b41f26aa1716f5b0aba90e4a7ae
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-02 10:49:01 +0100

    Create Aggregation.sql

---
+++ PostgreSQL Exercises/Aggregation.sql
@@ -0,0 +1,32 @@
+--Queries based on exercises from PostgreSQL Exercises (https://pgexercises.com/questions/basic/)
+
+/*
+Aggregation is one of those capabilities that really make you appreciate the power of relational database systems. 
+It allows you to move beyond merely persisting your data, into the realm of asking truly interesting questions that can be used to inform decision making. 
+This category covers aggregation at length, making use of standard grouping as well as more recent window functions.
+*/
+
+/*
+Count the number of facilities
+Question:
+For our first foray into aggregates, we're going to stick to something simple. We want to know how many facilities exist - simply produce a total count.f
+*/
+--Answer 
+SELECT COUNT(*) FROM cd.facilities; 
+
+/*
+
+Question:
+
+
+*/
+--Answer 
+
+
+/*
+
+Question:
+
+
+*/
+--Answer 
