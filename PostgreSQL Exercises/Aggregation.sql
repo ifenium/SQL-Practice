03004cab0b244da25ff05219d74d84581dc76f1e
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-05 12:13:01 +0100

    Update Aggregation.sql

--- PostgreSQL Exercises/Aggregation.sql
+++ PostgreSQL Exercises/Aggregation.sql
@@ -218,7 +218,16 @@ Question:
 Produce a list of member names, with each row containing the total member count. Order by join date, and include guest members.
 */
 --Answer 
+SELECT (SELECT COUNT(*) FROM cd.members as count), firstname, surname 
+	FROM cd.members 
+ORDER BY joindate;
 
+--or 
+
+SELECT COUNT(*) OVER(), firstname, surname
+	FROM cd.members
+ORDER BY joindate;
+																					 
 /*
 
 Question:
