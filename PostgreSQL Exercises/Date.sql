044393e1839c5974b1de5d20f51c72a52301e198
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-07 14:19:14 +0100

    Create Date.sql

---
+++ PostgreSQL Exercises/Date.sql
@@ -0,0 +1,36 @@
+  
+--Queries based on exercises from PostgreSQL Exercises (https://pgexercises.com/questions/basic/)
+
+/*
+Working with Timestamps
+Dates/Times in SQL are a complex topic, deserving of a category of their own. 
+They're also fantastically powerful, making it easier to work with variable-length concepts like 'months' than many programming languages. 
+*/
+
+/*
+
+-- Question:
+
+*/
+-- Answer
+
+/*
+
+-- Question:
+
+*/
+-- Answer
+
+/*
+
+-- Question:
+
+*/
+-- Answer
+
+/*
+
+-- Question:
+
+*/
+-- Answer
