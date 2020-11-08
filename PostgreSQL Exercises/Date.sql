bbd0b9b53c525e3f40dac208d2aca500d6c28260
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-08 16:49:50 +0100

    Update Date.sql

--- PostgreSQL Exercises/Date.sql
+++ PostgreSQL Exercises/Date.sql
@@ -42,13 +42,56 @@ SELECT EXTRACT(DAY from timestamp '2012-08-31');
 
 /*
 Work out the number of seconds between timestamps
--- Question:
+Question:
 Work out the number of seconds between the timestamps '2012-08-31 01:00:00' and '2012-09-02 00:00:00'
 */
 -- Answer
 SELECT EXTRACT(EPOCH FROM timestamp '2012-09-02 00:00:00'- timestamp '2012-08-31 01:00:00') AS date_part
 
+/*
+Work out the number of days in each month of 2012
+-- Question:
+For each month of the year in 2012, output the number of days in that month. 
+Format the output as an integer column containing the month of the year, and a second column containing an interval data type.
+*/
+-- Answer
+
+SELECT EXTRACT(MONTH FROM cal.month) AS month, 
+		(cal.month + interval '1 month') - cal.month AS length
+FROM (
+	SELECT generate_series(timestamp '2012-01-01', timestamp '2012-12-01', interval '1 month') AS month
+  ) AS cal
+ORDER BY month;
+
+--or
+-- Using CTE
+WITH cal AS (
+  SELECT * 
+  FROM generate_series(timestamp '2012-01-01', timestamp '2012-12-01', interval '1 month') AS month
+ )
+ 
+SELECT EXTRACT(MONTH FROM cal.month) AS month, 
+		(cal.month + interval '1 month') - cal.month AS length
+FROM cal
+ORDER BY month;
+
+/*
 -- Question:
 
 */
 -- Answer
+
+
+/*
+-- Question:
+
+*/
+-- Answer
+
+
+/*
+-- Question:
+
+*/
+-- Answer
+
