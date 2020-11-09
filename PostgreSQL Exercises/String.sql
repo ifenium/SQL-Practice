67b756140d678606aa5ee23419243436d1f6ac70
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-09 11:09:44 +0100

    Create String.sql

---
+++ PostgreSQL Exercises/String.sql
@@ -0,0 +1,50 @@
+  
+--Queries based on exercises from PostgreSQL Exercises (https://pgexercises.com/questions/basic/)
+
+/*
+String Operations
+String operations in most RDBMSs are, arguably, needlessly painful. 
+Fortunately, Postgres is better than most in this regard, providing strong regular expression support. 
+This section covers basic string manipulation, use of the LIKE operator, and use of regular expressions. 
+I also make an effort to show you some alternative approaches that work reliably in most RDBMSs. 
+*/
+
+/*
+
+Question: 
+
+*/ 
+-- Answer
+
+
+/*
+
+Question: 
+
+*/ 
+-- Answer
+
+
+/*
+
+Question: 
+
+*/ 
+-- Answer
+
+
+/*
+
+Question: 
+
+*/ 
+-- Answer
+
+
+/*
+
+Question: 
+
+*/ 
+-- Answer
+
