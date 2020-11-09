a87b788e9587d9b03b3de82ebd6c46b17741cbf5
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-09 12:36:38 +0100

    Update String.sql

--- PostgreSQL Exercises/String.sql
+++ PostgreSQL Exercises/String.sql
@@ -58,10 +58,39 @@ SELECT memid, telephone
 SELECT memid, telephone 
   FROM cd.members
   WHERE telephone ~ '[()]';
+
 /*
-
+Pad zip codes with leading zeroes
 Question: 
-
+The zip codes in our example dataset have had leading zeroes removed from them by virtue of being stored as a numeric type.
+Retrieve all zip codes from the members table, padding any zip codes less than 5 characters long with leading zeroes. 
+Order by the new zip code.
 */ 
 -- Answer
+SELECT LPAD(CAST(zipcode AS char(5)), 5, '0') AS zip 
+  FROM cd.members
+  ORDER BY zip;
 
+/*
+Count the number of members whose surname starts with each letter of the alphabet
+Question: 
+You'd like to produce a count of how many members you have whose surname starts with each letter of the alphabet. 
+Sort by the letter, and don't worry about printing out a letter if the count is 0.
+*/ 
+-- Answer
+SELECT SUBSTR(mems.surname,1,1) AS letter, count(*) AS count
+  FROM cd.members AS mems 
+  GROUP BY letter
+  ORDER BY letter;
+                 
+/*
+Clean up telephone numbers
+Question: 
+The telephone numbers in the database are very inconsistently formatted. 
+You'd like to print a list of member ids and numbers that have had '-','(',')', and ' ' characters removed. Order by member id.
+*/ 
+-- Answer
+SELECT memid, TRANSLATE(telephone, '-() ', '') AS telephone
+  FROM cd.members 
+  GROUP BY memid
+  ORDER BY memid;
