bf2eb054bbbe91a2c32b99277400864a0e1e50e0
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-09 11:52:58 +0100

    Update String.sql

--- PostgreSQL Exercises/String.sql
+++ PostgreSQL Exercises/String.sql
@@ -10,37 +10,54 @@ I also make an effort to show you some alternative approaches that work reliably
 */
 
 /*
-
+Format the names of members
 Question: 
-
+Output the names of all members, formatted as 'Surname, Firstname'
 */ 
 -- Answer
-
+SELECT surname || ', ' || firstname AS name
+FROM cd.members;
 
 /*
-
+Find facilities by a name prefix
 Question: 
-
+Find all facilities whose name begins with 'Tennis'. Retrieve all columns.
 */ 
 -- Answer
-
+SELECT *  
+  FROM cd.facilities
+  WHERE name LIKE 'Tennis%';
 
 /*
-
+Perform a case-insensitive search
 Question: 
-
+Perform a case-insensitive search to find all facilities whose name begins with 'tennis'. Retrieve all columns.
 */ 
 -- Answer
-
+SELECT *  
+  FROM cd.facilities
+  WHERE upper(name) LIKE 'TENNIS%';
 
 /*
-
+Find telephone numbers with parentheses
 Question: 
-
+You've noticed that the club's member table has telephone numbers with very inconsistent formatting. 
+You'd like to find all the telephone numbers that contain parentheses, returning the member ID and telephone number sorted by member ID.
 */ 
 -- Answer
+SELECT memid, telephone 
+  FROM cd.members
+  WHERE telephone LIKE '(%';
 
+--or
+SELECT memid, telephone 
+  FROM cd.members
+  WHERE telephone SIMILAR TO '%[()]%';
 
+--or (using Regular Expressions)
+SELECT memid, telephone 
+  FROM cd.members
+  WHERE telephone ~ '[()]';
 /*
 
 Question: 
