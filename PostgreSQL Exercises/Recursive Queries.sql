f7021fcc63ef9bed6b3fc594f5af5d6b68fc83d9
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-10 16:15:00 +0100

    Create Recursive Queries.sql

---
+++ PostgreSQL Exercises/Recursive Queries.sql
@@ -0,0 +1,43 @@
+--Queries based on exercises from PostgreSQL Exercises (https://pgexercises.com/questions/recursive/)
+
+/*
+Common Table Expressions allow us to, effectively, create our own temporary tables for the duration of a query - 
+they're largely a convenience to help us make more readable SQL. 
+Using the WITH RECURSIVE modifier, however, it's possible for us to create recursive queries. 
+This is enormously advantageous for working with tree and graph-structured data - 
+imagine retrieving all of the relations of a graph node to a given depth, for example.
+*/
+
+/*
+
+Question:
+
+*/
+--Answer 
+
+
+
+/*
+
+Question:
+
+*/
+--Answer 
+
+
+/*
+
+Question:
+
+*/
+--Answer 
+
+
+
+/*
+
+Question:
+
+*/
+--Answer 
+
