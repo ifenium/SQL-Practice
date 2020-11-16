f755c9fa9d2267cfa9e24e9c42f8683c80ac565b
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-16 22:32:49 +0100

    Update Intro to SQL: Modifying databases with SQL.sql

--- Khan Academy/Intro to SQL: Modifying databases with SQL.sql
+++ Khan Academy/Intro to SQL: Modifying databases with SQL.sql
@@ -34,5 +34,34 @@ SELECT *
 FROM diary_logs;
 
 
+/*
+Challenge:
+Dynamic Documents
+*/
+
+CREATE table documents (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    content TEXT,
+    author TEXT);
+    
+INSERT INTO documents (author, title, content)
+    VALUES ("Puff T.M. Dragon", "Fancy Stuff", "Ceiling wax, dragon wings, etc.");
+INSERT INTO documents (author, title, content)
+    VALUES ("Puff T.M. Dragon", "Living Things", "They're located in the left ear, you know.");
+INSERT INTO documents (author, title, content)
+    VALUES ("Jackie Paper", "Pirate Recipes", "Cherry pie, apple pie, blueberry pie.");
+INSERT INTO documents (author, title, content)
+    VALUES ("Jackie Paper", "Boat Supplies", "Rudder - guitar. Main mast - bed post.");
+INSERT INTO documents (author, title, content)
+    VALUES ("Jackie Paper", "Things I'm Afraid Of", "Talking to my parents, the sea, giant pirates, heights.");
+
+SELECT * FROM documents;
+
+-- Use UPDATE to change the author to 'Jackie Draper' for all rows where it's currently 'Jackie Paper'. 
+-- Then re-select all the rows to make sure the table changed like you expected.
+UPDATE documents 
+SET author="Jackie Draper" 
+WHERE author="Jackie Paper";
 
 
