b83031a1574df7dec0e9bca280c336925b107935
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-17 20:56:02 +0100

    Update Intro to SQL: Modifying databases with SQL.sql

--- Khan Academy/Intro to SQL: Modifying databases with SQL.sql
+++ Khan Academy/Intro to SQL: Modifying databases with SQL.sql
@@ -103,10 +103,39 @@ SELECT * FROM diary_logs;
 -- Deletes the entire table 
 DROP TABLE diary_logs;
 
+/*
+Challenge:
+Clothing alterations
+*/
 
+CREATE TABLE clothes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    design TEXT);
+    
+INSERT INTO clothes (type, design)
+    VALUES ("dress", "pink polka dots");
+INSERT INTO clothes (type, design)
+    VALUES ("pants", "rainbow tie-dye");
+INSERT INTO clothes (type, design)
+    VALUES ("blazer", "black sequin");
 
+-- Use ALTER to add a 'price' column to the table. Then select all the columns in each row to see what your table looks like now.
+ALTER TABLE clothes ADD price INTEGER;
+SELECT * FROM clothes;
 
+-- Assign each item a price, using UPDATE - item 1 should be 10 dollars, item 2 should be 20 dollars, item 3 should be 30 dollars. 
+-- When you're done, do another SELECT of all the rows to check that it worked as expected.
+UPDATE clothes SET price = 10 WHERE id=1;
+UPDATE clothes SET price = 20 WHERE id=2;
+UPDATE clothes SET price = 30 WHERE id=3;
 
+SELECT * FROM clothes;
 
+-- Insert a new item into the table that has all three attributes filled in, including 'price'. Do one final SELECT of all the rows to check it worked.
+INSERT INTO clothes (type, design, price)
+    VALUES("T-Shirt", "plain", 40);
+    
+SELECT * FROM clothes
 
 
