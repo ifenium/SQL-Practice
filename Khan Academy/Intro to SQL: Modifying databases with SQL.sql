ee52dbf536767240f556fb99876835db4265d5bf
Ifeoluwa Oyelakin <oyeife@gmail.com>
2020-11-17 20:44:58 +0100

    Update Intro to SQL: Modifying databases with SQL.sql

--- Khan Academy/Intro to SQL: Modifying databases with SQL.sql
+++ Khan Academy/Intro to SQL: Modifying databases with SQL.sql
@@ -68,3 +68,45 @@ WHERE author="Jackie Paper";
 DELETE FROM documents 
 WHERE title = "Things I'm Afraid Of";
 SELECT * FROM documents;
+
+/*
+Altering tables after creation
+*/
+
+/* What we used to originally create the table */
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT);
+    
+CREATE TABLE diary_logs (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    date TEXT,
+    content TEXT
+    );
+    
+/* After user submits a diary log */
+INSERT INTO diary_logs (user_id, date, content) VALUES (1, "2015-04-02",
+    "OhNoesGuy and I made up and now we're best friends forever and we celebrated with a tub of ice cream.");
+    
+-- Adds a new column "emotion" to the table
+ALTER TABLE diary_logs ADD emotion TEXT;
+
+--  Modifies the table's scheme and sets deault value to "unknown" instead of default
+ALTER TABLE diary_logs ADD emotion TEXT default "unknown";
+
+INSERT INTO diary_logs (user_id, date, content, emotion) VALUES (1, "2015-04-03",
+    "We went to Disneyland!", "happy");
+    
+SELECT * FROM diary_logs;
+
+-- Deletes the entire table 
+DROP TABLE diary_logs;
+
+
+
+
+
+
+
+
